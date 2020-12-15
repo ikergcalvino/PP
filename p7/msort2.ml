2113bc3b85910d4a9debe14da0ddc9a4f55ffeca
Íker García Calviño <iker.gcalvino@udc.es>
2020-12-15 22:38:02 +0100

    merge update

--- p7/msort2.ml
+++ p7/msort2.ml
@@ -3,15 +3,15 @@ let rec divide = function
   | h::[] -> [h], []
   | h1::h2::t -> let t1, t2 = divide t in (h1::t1, h2::t2);;
 
-let rec merge ord l1 l2 = match l1, l2 with
+let rec merge ord = function
   [], l | l, [] -> l
-  | h1::t1, h2::t2 -> if ord h1 h2 then h1 :: merge ord t1 l2
-                      else h2 :: merge ord l1 t2;;
+  | h1::t1, h2::t2 -> if (ord h1) h2 then h1 :: merge ord (t1, h2::t2)
+                      else h2 :: merge ord (h1::t1, t2);;
 
 let rec msort1 ord l = match l with
   [] | _::[] -> l
   | _ -> let l1, l2 = divide l in
-        merge ord (msort1 ord l1) (msort1 ord l2);;
+        merge ord (msort1 ord l1, msort1 ord l2);;
 
 (* Puede ocurrir Stack Overflow, por ejemplo con: divide [1..256000] *)
 
@@ -31,8 +31,8 @@ let divide' l =
     | h1::h2::t -> aux (h1::dvd1) (h2::dvd2) t
   in aux [] [] l;;
 
-let merge' ord l1 l2 =
-  let rec aux mer = function
+let merge' ord (l1, l2) =
+  let rec aux (a1, a2) mer = match a1, a2 with
     [], l | l, [] -> List.rev_append mer l
     | h1::t1, h2::t2 -> if ord h1 h2 then aux (h1::mer) (t1, h2::t2)
                         else aux (h2::mer) (h1::t1, t2)
@@ -41,7 +41,7 @@ let merge' ord l1 l2 =
 let rec msort2 ord l = match l with
   [] | _::[] -> l
   | _ -> let l1, l2 = divide' l
-in merge' ord (msort2 ord l1) (msort2 ord l2);;
+in merge' ord (msort2 ord l1, msort2 ord l2);;
 
 (* msort2 es un poco más lento que msort1 pero mucho más rápido que qsort2 *)
 (* - para msort2 ordenar [1..10000] le lleva sobre 0.008646 *)
