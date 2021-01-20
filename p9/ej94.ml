31236d80f9b96cbc9133a6dd1090e4fabfb60bb1
Íker García Calviño <iker.gcalvino@udc.es>
2021-01-20 17:38:26 +0100

    ej94 bug fixed

--- p9/ej94.ml
+++ p9/ej94.ml
@@ -1,16 +1,9 @@
 open St_tree;;
 
-exception Branches;;
-
-let branches tree = match tree with
-    C (tree, d, i) -> (d, i)
-  | S h -> raise (Branches);;
-
 let is_single t =
-  try let_ = branches t in false
-  with Branches -> true;;
+  try let _ = branches t in false
+  with No_branches -> true;;
 
 let rec breadth_first t = match is_single t with
     true -> [root t]
-  | false -> let l, r = branches t
-in (root t :: (List.append (breadth_first l) (breadth_first r)));;
+  | false -> let l, r = branches t in (root t :: (List.append (breadth_first l) (breadth_first r)));;
